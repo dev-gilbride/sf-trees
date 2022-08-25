20127ff3743e9dd69b2e1ea051f5589fa278043e
dev-gilbride <ee.bgilbride@gmail.com>
2022-08-24 18:52:14 -0600

    Implements asyncio queue

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import aiohttp
 import asyncio
 import argparse
 import geopandas as gpd
 from geopy.adapters import AioHTTPAdapter
 import geopy.geocoders
 from geopy.geocoders import Nominatim
-import json
 import logging
+import math
 import pandas as pd
 from shapely.geometry import Point
 import urllib.request
-import socket
 
 
 async def get_address_coords(address: str) -> dict:
@@ -21,7 +21,7 @@ async def get_address_coords(address: str) -> dict:
         adapter_factory=AioHTTPAdapter,
     ) as geolocator:
         attempts = 0
-        while attempts < 3:
+        while attempts < 5:
             try:
                 location = await geolocator.geocode(address)
                 return {
@@ -32,12 +32,12 @@ async def get_address_coords(address: str) -> dict:
                     geopy.exc.GeocoderUnavailable,
                     geopy.exc.GeocoderServiceError,
                     geopy.exc.GeocoderQuotaExceeded) as err:
-                logging.warning(f'Attempt {attempts + 1} failed with error: {err}')
+                logging.warning(f'Attempt {attempts + 1} failed with error {err}')
                 attempts += 1
         raise ConnectionError("Failed to retrieve coordinates for the address.")
 
 
-def get_sf_tree_data(page_size: int, offset: int = 0):
+async def get_sf_tree_data(page_size: int, offset: int) -> dict:
     """Queries the sf-trees datasette for a page of entries with offset."""
 
     SF_TREES_URL = "https://san-francisco.datasettes.com/sf-trees"
@@ -50,32 +50,26 @@ def get_sf_tree_data(page_size: int, offset: int = 0):
         query = query + f' offset {offset}'
     query = urllib.parse.quote_plus(query)
     url = SF_TREES_URL + '.json?sql=' + query
-    logging.debug(f'Obtaining query: {url}')
-    req = urllib.request.Request(
-        url,
-        data=None,
-        # Adds user headers to avoid rate limiting.
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-        }
-    )
+    logging.debug(f'Request Offset {offset}: Obtaining query: {url}')
     attempts = 0
-    while attempts < 5:
+    while attempts < 10:
         try:
-            with urllib.request.urlopen(req, timeout=3) as response:
-                if response.status != 200:
-                    raise urllib.error.URLError(f'Query returned a bad reponse status: {response.status}')
-                data = json.load(response)
-                return data
-        except (socket.timeout,
-                urllib.error.urlerror,
-                urllib.error.HTTPError) as err:
-            logging.warning(f'Attempt {attempts + 1} failed with error: {err}')
+            timeout = aiohttp.ClientTimeout(total=3)
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                async with session.get(url) as resp:
+                    if resp.status != 200:
+                        raise aiohttp.ClientConnectorError(f'Query returned a bad reponse status: {resp.status}')
+                    data = await resp.json()
+                    logging.debug(f'Data obtained for offset: {offset}')
+                    return data
+        except (asyncio.exceptions.TimeoutError,
+                aiohttp.ClientConnectorError) as err:
+            logging.warning(f'Request Offset {offset}: Attempt {attempts + 1} failed with error {err}')
             attempts += 1
-        raise ConnectionError(f'Failed to retrieve url: {url}')
+    raise ConnectionError(f'Failed to retrieve url: {url}')
 
 
-def filter_by_proximity(center: dict, radius: float, data: dict):
+async def filter_by_proximity(center: dict, radius: float, data: dict, query_offset: int) -> gpd.GeoDataFrame:
     """Converts data into a GeoDataFrame and returns entries within radius distance of the center."""
 
     df = pd.DataFrame(data["rows"])
@@ -86,7 +80,7 @@ def filter_by_proximity(center: dict, radius: float, data: dict):
         df,
         geometry=gpd.points_from_xy(
             df.Longitude, df.Latitude, crs="EPSG:4326"))
-    logging.debug(f'GeoDataFrame entry count: {len(gdf.index)}')
+    logging.debug(f'Request Offset {query_offset}: GeoDataFrame entry count: {len(gdf.index)}')
     center_point = Point(center['Longitude'], center['Latitude'])
     center_point_geodetic = gpd.GeoSeries([center_point], crs="EPSG:4326")
     center_point_cartesian = center_point_geodetic.to_crs(crs="EPSG:3857").iloc[0]
@@ -95,42 +89,70 @@ def filter_by_proximity(center: dict, radius: float, data: dict):
     return (gdf[df["proximity"] <= radius])
 
 
+async def consumer(name: int, q: asyncio.Queue, center: dict, radius: float, page_size: int) -> list:
+    """Consumer for query requests in queue based off an offset value. Returns list of GeoDataFrames accrued upon reaching an empty query result."""
+
+    trees_to_add = []
+    while True:
+        query_offset = await q.get()
+        data = await get_sf_tree_data(page_size, query_offset)
+        if len(data["rows"]) <= 0:
+            logging.debug(f'Consumer ID {name} Request Offset {query_offset}: Reached empty result. Returning values and exiting.')
+            return trees_to_add
+        logging.debug(f'Consumer ID {name} Request Offset {query_offset}: Data collected from URL count: {len(data["rows"])}')
+        trees = await filter_by_proximity(center, radius, data, query_offset)
+        logging.debug(f'Consumer ID {name} Request Offset {query_offset}: Filtered tree count: {len(trees.index)}')
+        trees_to_add.append(trees)
+        q.task_done()
+
+
+async def producer(q: asyncio.Queue, page_size: int) -> None:
+    """Producer for query requests in queue based off an offset value. Waits on queue size to implement more."""
+
+    query_offset = 0
+    while True:
+        logging.debug(f'Request offset {query_offset} added to queue.')
+        await q.put(query_offset)
+        query_offset += page_size
+
+
+async def main(address: str, blocks: int, block_length: float, page_size: int, logging: str, runners: int):
+    """Producer for query requests in queue based off an offset value. Waits on queue size to implement more."""
+    queue_size = math.ceil(runners * 1.5)
+    q = asyncio.Queue(maxsize=queue_size)
+
+    center_coordinates = await get_address_coords(address)
+    radius = block_length * blocks
+
+    producer_task = asyncio.create_task(producer(q, page_size))
+    consumers = [asyncio.create_task(consumer(name, q, center_coordinates, radius, page_size)) for name in range(runners)]
+    returned_trees = await asyncio.gather(*consumers)
+    producer_task.cancel()
+
+    trees_to_add = [df for sublist in returned_trees for df in sublist]
+    trees_in_range = pd.concat(trees_to_add)
+    trees_in_range.reset_index(drop=True, inplace=True)
+
+    print(f'There are {len(trees_in_range.index)} trees within a {radius}m radius.')
+    print(f'Where the radius consists of {blocks} blocks of length {block_length}m.')
+    print(f'Centered around address: {address}')
+    if len(trees_in_range.index) > 0:
+        print(trees_in_range)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find trees with a block radius of given address.")
     parser.add_argument("--address", type=str, help="Address to center the search around.")
     parser.add_argument("--blocks", type=int, help="Number of blocks to extend search radius.")
     parser.add_argument("--block-length", required=False, default=182.88, type=float,
                         help="Length in meters to measure a block. Defaults to US average of 182.88m.")
-    parser.add_argument("--page-size", required=False, choices=range(100,1000), default=1000, type=int, metavar="[100-1000]", 
-                        help="Number of database entries per request up to 1000. Defaults to 1000.")
+    parser.add_argument("--page-size", required=False, choices=range(100, 1000), default=1000, type=int, metavar="[100-1000]",
+                        help="Number of database entries per request. Defaults to 1000.")
     parser.add_argument("--logging", required=False, default='info', type=str, help="Logging mode. Defaults to INFO.")
+    parser.add_argument("--runners", required=False, default=20, type=int, help="Number of runners to consume from queue.")
     args = parser.parse_args()
 
     logging_level = getattr(logging, args.logging.upper(), None)
     logging.basicConfig(level=logging_level)
 
-    center_coordinates = asyncio.run(get_address_coords(args.address))
-
-    radius = args.block_length * args.blocks
-    offset = 0
-    trees_to_add = []
-    while True:  # Do While Loop
-        data = get_sf_tree_data(args.page_size, offset)
-        if len(data["rows"]) > 0:
-            logging.debug(f'Data entry count: {len(data["rows"])}')
-            offset += args.page_size
-            trees = filter_by_proximity(center_coordinates, radius, data)
-            logging.debug(f'Filtered data count: {len(trees.index)}')
-            trees_to_add.append(trees)
-            logging.debug(f'DataFrames appended: {len(trees_to_add)}')
-        else:
-            break
-    trees_in_range = pd.concat(trees_to_add)
-    trees_in_range.reset_index(drop=True, inplace=True)
-
-    print(f'There are {len(trees_in_range.index)} trees within a {radius}m radius.')
-    print(f'Where the radius consists of {args.blocks} blocks of length {args.block_length}m.')
-    print(f'Centered around address: {args.address}')
-    if len(trees_in_range.index) > 0:
-        print(trees_in_range)
-        trees_in_range.to_csv(f'{args.page_size}_trees.csv', index=False)
+    asyncio.run(main(**args.__dict__))
